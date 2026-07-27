f853f856bf6f05bb66c8532257cf190e7e83213b
josuejx <josue.ga00@gmail.com>
2026-07-27 10:06:00 +0200

    feat: add HeroSection content with headline, description and CTA buttons

--- src/components/content/HeroSection.vue
+++ src/components/content/HeroSection.vue
@@ -1,5 +1,18 @@
 <script setup></script>
 
 <template>
-    <section></section>
+    <section class="flex items-center justify-around h-[80vh] border-b-2 border-gray-500/30">
+        <div class="flex flex-col gap-4 w-1/2">
+            <p class="uppercase text-[#B399D2]">// IA Autónoma para PYMES y pequeños negocios</p>
+            <h1 class="text-6xl font-bold text-white">El tiempo de tu empresa es ahora.</h1>
+            <p>Las PYMEs son las que menos tiempo tienen y más ocupadas están. Integración de un agente IA a medida que entiende la lógica real de tu negocio, asume las operaciones diarias y asesora la toma de decisiones.</p>
+
+            <div class="flex gap-4">
+                <button class="bg-custom-primary text-black font-bold px-4 py-2">[ SOLICITAR AUDITORÍA IA ]</button>
+                <button class="bg-custom-accent border font-bold px-4 py-2">> EXPLORAR CONCEPTO</button>
+            </div>
+        </div>
+
+        <div class="w-1/2"></div>
+    </section>
 </template>
